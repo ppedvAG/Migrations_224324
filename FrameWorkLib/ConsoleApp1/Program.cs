7a7c3c5ca011afcb7ad4532e75ceb8a5fc196a20
André <AndreR@ppedv.de>
2023-03-07 10:10:16 +0100

    .NET Framwork 4.8 Lib + .NET 7

---
+++ FrameWorkLib/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FrameWorkLib;
+
+Console.WriteLine("Hello, World!");
+
+Class1 class1 = new Class1();
+
+class1.MyProperty = 1;
