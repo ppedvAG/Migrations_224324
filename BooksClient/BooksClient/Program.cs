3b5900fb517832da710e37122d43756555da719e
André <AndreR@ppedv.de>
2023-03-07 16:07:03 +0100

    BooksClient

---
+++ BooksClient/BooksClient/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BooksClient;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+
+var url = "https://www.googleapis.com/books/v1/volumes?q=dotnet";
+
+var http = new HttpClient();
+var json = await http.GetStringAsync(url);
+
+var books = JsonSerializer.Deserialize<BookResult>(json);
+
+//Console.WriteLine(json);
+
+foreach (var book in books.items)
+    Console.WriteLine(book.volumeInfo.title);
