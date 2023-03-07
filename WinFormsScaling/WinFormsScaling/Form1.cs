91cf8091067e2f0df1bc1539ac403dc486a81297
André <AndreR@ppedv.de>
2023-03-07 10:24:28 +0100

    Framework anzeige

--- WinFormsScaling/WinFormsScaling/Form1.cs
+++ WinFormsScaling/WinFormsScaling/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,7 @@ namespace WinFormsScaling
         public Form1()
         {
             InitializeComponent();
+            button2.Text = RuntimeInformation.FrameworkDescription;
         }
     }
 }
