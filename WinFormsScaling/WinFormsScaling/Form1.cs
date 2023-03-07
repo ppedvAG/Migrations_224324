5396c4ea5f5b120349ae8ae2c38b20c1c84e6050
André <AndreR@ppedv.de>
2023-03-07 11:08:22 +0100

    AppSettings.json als Config

--- WinFormsScaling/WinFormsScaling/Form1.cs
+++ WinFormsScaling/WinFormsScaling/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,21 @@ namespace WinFormsScaling
         public Form1()
         {
             InitializeComponent();
+
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", true)
+                                                              .AddUserSecrets(Assembly.GetExecutingAssembly())
+                                                              .Build();
+
+            bool isCool = config.GetSection("IsCool").Value == "True";
+            var conStrings = config.GetSection("ConnectionStrings");
+            var conStringDb1 = conStrings.GetSection("db1").Value;
+
+            if (isCool)
+            {
+                BackColor = Color.SteelBlue;
+            }
+
+            textBox1.Text = conStringDb1;
         }
     }
 }
